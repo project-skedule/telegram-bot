2a091ff7c77f2cc0e30e7e508234401dacb0fdd5
Anton Kudryavtsev <antonkudryavtsevdoem@gmail.com>
2021-12-28 20:22:18 +0300

    Dev (#26)
    * merge
    
    * update depedencies
    
    * Init images dir
    
    * added function for creating week timetable
    
    * width and height replaced
    
    * Deps
    
    * gitignore
    
    * Update deps
    
    * Black format
    
    * Update script
    
    * Black Format
    
    * Update .gitignore
    
    * надо возвращать id кабинетов при получении всех кабинетов
    Fixes #20
    
    * добавить возможность изменить кабинет при изменении урока
    Fixes #19
    
    * Fix сделать проверку на то, что данные при изменении урока из одной школы #18
    
    * Fix checking school
    
    * Change pipfile to requirements
    
    * Imports fix
    
    Co-authored-by: saveliyh <savel.khlebnov@yandex.ru>
    Co-authored-by: Anton Domozhirov <60037937+mrcatcis@users.noreply.github.com>

--- src/api.py
+++ src/api.py
@@ -192,6 +192,7 @@ async def get_student_week(telegram_id, student_id):
 
     data = {"subclass_id": student_id}
 
+
     data = await get_request(
         "/lesson/get/range",
         data={
@@ -281,6 +282,22 @@ async def get_user_week(
             return await get_student_week(
                 telegram_id, await get_subclass_id(telegram_id)
             )
+
+    lessons = data["lessons"]
+
+    result = ""
+    for lesson in lessons:
+        number = lesson["lesson_number"]
+        result += (
+            f"Урок №{number['number']} {number['time_start']} - {number['time_end']}\n"
+        )
+        result += f"Предмет: *{lesson['subject']}*\n"
+        result += f"{lesson['teacher']['name']}\n"
+        result += f"{lesson['corpus']['name']}, {lesson['cabinet']['name']}\n"
+        result += f"\n"
+
+    return result
+
 
     lessons = data["lessons"]
 
